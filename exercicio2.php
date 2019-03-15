2ba67c573b8e0bf865d53e3b684592067573c4fe
Isadora Marutti <48135727+isadoramarutti@users.noreply.github.com>
2019-03-15 16:06:29 -0300

    exercício finalizado

---
+++ exercicio2.php
@@ -0,0 +1,21 @@
+<?php
+
+print "Quantos dias você deseja converter?: ";
+$dias =  fgets (STDIN);
+
+print "Quantas horas você deseja converter?: ";
+$horas =  fgets (STDIN);
+
+print "Quantos minutos você deseja converter?: ";
+$minutos =  fgets (STDIN);
+
+print "Quantos segundos você deseja converter?: ";
+$segundos = fgets (STDIN);
+
+$dias_convertidos = $dias*86400; 
+$horas_convertidos = $horas*3600;
+$minutos_convertidos = $minutos*60;
+
+$total_segundos = $dias_convertidos + $horas_convertidos + $minutos_convertidos + $segundos ; 
+
+print "O total de segundos que $dias dias, $horas horas, $minutos minutos e $segundos segundos possui é de $total_segundos segundos.";
